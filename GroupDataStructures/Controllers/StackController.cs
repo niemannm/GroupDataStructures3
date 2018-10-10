272a36453fcd4466c2f799c8e2b40e65c550afe9
SAMGINES135A\samgines <sam.gines.104@gmail.com>
2018-10-10 16:36:42 -0600

    changes

--- GroupDataStructures/Controllers/StackController.cs
+++ GroupDataStructures/Controllers/StackController.cs
@@ -22,7 +22,7 @@ namespace GroupDataStructures.Controllers
         public ActionResult addToStack()
         {
             ++iStackCount;
-            myStack.Push("New Entry " + iStackCount + " ");
+            myStack.Push("New Entry " + iStackCount + " n\");
 
             return View("Index");
         }
@@ -34,7 +34,7 @@ namespace GroupDataStructures.Controllers
 
             for (iStackCount = 0; iStackCount < 2001; ++iStackCount)
             {
-                myStack.Push("New Entry " + iStackCount + " ");
+                myStack.Push("New Entry " + iStackCount + " \n");
             }
 
             return View("Index");
